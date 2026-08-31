64165202497b7a92173543ebd2a54e9c563cf9b8
Zhou Cong <yinoo0@163.com>
2026-08-31 19:12:29 +0800

    fix: enable mobile viewport layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -46,3 +46,8 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
